b2cb307e4020489e97f550ad67542df0eb24aa75
Connor Brereton <breretonconnor@gmail.com>
2023-05-02 15:27:14 -0700

    Update api.py to fix breaking changes and also use .env for API_KEY

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 headers = {
-    "authorization": '',
+    "authorization": os.getenv('API_KEY'),
     "content-type": "application/json"
 }
 
@@ -25,10 +25,10 @@ def upload_transcribe(audio_file):
 
     json = {
         "audio_url": audio_url,
-        "word_boost": ["how's", "how is", "how are", "how", "what's", "what is", "what are", "what", "when", "where", "why", "who", "which", "whose", "whom", "do", "does", "did", "done", "doing", "have", "has", "had", "having", "be", "is", "are", "was", "were", "being", "been", "can", "could", "may", "might", "must", "shall", "should", "will", "would", "ought", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"],
+        "word_boost": ["how's", "how is", "how are", "how", "what's", "what is", "what are", "what", "when", "where", "why", "who", "which", "whose", "whom", "do", "does", "did", "done", "doing", "have", "has", "had", "having", "be", "is", "are", "was", "were", "being", "been", "can", "could", "may", "might", "must", "shall", "should", "will", "would", "ought", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "sales intelligence"],
         "boost_param": "high",
+        "dual_channel": True,
         "iab_categories": True,
-        "speaker_labels": True,
         "auto_highlights": True
     }
 
@@ -36,5 +36,5 @@ def upload_transcribe(audio_file):
     print(response.json())
     transcript_id = response.json()['id']
     polling_endpoint = transcript_endpoint + "/" + transcript_id
-
+    
     return polling_endpoint
