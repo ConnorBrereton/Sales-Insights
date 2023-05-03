cdd02fbdcda7555c4529323df7ad61c18e63d68a
Connor Brereton <breretonconnor@gmail.com>
2023-05-03 16:03:13 -0700

    Update api.py - included documentation
    where it might be necessary. most of the variables and procedures are self explanatory.

--- api.py
+++ api.py
@@ -14,15 +14,23 @@ def upload_transcribe(audio_file):
     transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
     upload_endpoint = 'https://api.assemblyai.com/v2/upload'
 
-    print('uploading')
+    print('The file is being uploaded to AssemblyAI infrastructure...')
     upload_response = requests.post(
         upload_endpoint,
         headers=headers, data=audio_file
     )
 
     audio_url = upload_response.json()['upload_url']
-    print('done')
+    print('Fetching the audio data...')
 
+    # Here we boost certain words to extract questions. This is similar to how Python spaCy's
+    # PhraseMatcher but an order of magnitude more accurate. Also, can ensemble different
+    # algorithms together via JSON parameters that are turned on.
+    # By boosting words we increase the AI's sensitivity to these phrases, therefore increasing
+    # the accuracy of these different question and answers extraction.
+    #
+    # We turn on dual channels to ensure that we are discerning the different people in the
+    # audio call who are speaking.
     json = {
         "audio_url": audio_url,
         "word_boost": ["how's", "how is", "how are", "how", "what's", "what is", "what are", "what", "when", "where", "why", "who", "which", "whose", "whom", "do", "does", "did", "done", "doing", "have", "has", "had", "having", "be", "is", "are", "was", "were", "being", "been", "can", "could", "may", "might", "must", "shall", "should", "will", "would", "ought", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "sales intelligence"],
@@ -33,7 +41,7 @@ def upload_transcribe(audio_file):
     }
 
     response = requests.post(transcript_endpoint, json=json, headers=headers)
-    print(response.json())
+    print('Parsing the transcript ID to run NLP on the transcript...)
     transcript_id = response.json()['id']
     polling_endpoint = transcript_endpoint + "/" + transcript_id
     
